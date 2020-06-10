80a8cd3c79c60ab1815de15e96749de3bb9f8c49
lei-hsia <leileetcodekaggle@163.com>
2020-06-10 09:36:56 +0800

    dao interface

---
+++ cloud-provider-payment8001/src/main/java/com/lei/springcloud/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package com.lei.springcloud.dao;
+
+import com.lei.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
