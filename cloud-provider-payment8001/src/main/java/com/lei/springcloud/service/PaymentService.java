03a49f31be9e5f8371b469a12c24df673cf2acf0
lei-hsia <leileetcodekaggle@163.com>
2020-06-10 11:31:18 +0800

    PaymentController, PaymentService, PaymentServiceImpl
    Controller: pass result to Frontend: type: CommonResult
    Service: inside backend: Controller-Service-Dao: no need to use CommonResult

--- cloud-provider-payment8001/src/main/java/com/lei/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/lei/springcloud/service/PaymentService.java
@@ -2,9 +2,8 @@ package com.lei.springcloud.service;
 
 import com.lei.springcloud.entities.Payment;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface PaymentService {
     public int create(Payment payment);
     public Payment getPaymentById(@Param("id") Long id);
--- cloud-provider-payment8001/src/main/java/com/lei/springcloud/service/PaymentServiceImpl.java
+++ cloud-provider-payment8001/src/main/java/com/lei/springcloud/service/PaymentServiceImpl.java
@@ -3,9 +3,11 @@ package com.lei.springcloud.service;
 import com.lei.springcloud.dao.PaymentDao;
 import com.lei.springcloud.entities.Payment;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+@Service
 public class PaymentServiceImpl implements PaymentService{
 
     @Resource
