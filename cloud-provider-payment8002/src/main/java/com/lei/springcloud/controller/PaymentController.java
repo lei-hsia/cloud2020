5700c38eee9b97cd0688853cdcb9e261cb97dffe
lei-hsia <leileetcodekaggle@163.com>
2020-06-16 17:02:12 +0800

    cloud-provider-payment8002:前面掉了classpath下mapper包下的XML文件，导致org.apache.ibatis.binding.BindingException

--- cloud-provider-payment8002/src/main/java/com/lei/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/lei/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.lei.springcloud.entities.CommonResult;
 import com.lei.springcloud.entities.Payment;
 import com.lei.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,6 +16,9 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     // 糊涂了: 为什么是本地的8001调用: http://localhost:8001/payment/get/31; 不是192.168.68.146
     // 因为那个只是数据库在别的地方，实际上IDEA启动的spring服务都是在本地运行的，所以本地浏览器测试
 
@@ -24,7 +28,7 @@ public class PaymentController {
         log.info("******* 返回结果： " + result);
 
         if (result > 0) {
-            return new CommonResult(200, "插入数据库成功", result);
+            return new CommonResult(200, "插入数据库成功, serverPort:"+serverPort, result);
         } else {
             return new CommonResult(444, "插入数据库失败", null);
         }
@@ -36,7 +40,7 @@ public class PaymentController {
         log.info("******* 返回结果： " + payment);
 
         if (payment != null) {
-            return new CommonResult(200, "查询成功", payment);
+            return new CommonResult(200, "查询成功serverPort:"+serverPort, payment);
         } else {
             return new CommonResult(444, "查询失败", null);
         }
