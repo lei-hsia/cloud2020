c83fc79d1caa3b751dc070f05b6a2e6f1afb0cbb
lei-hsia <leileetcodekaggle@163.com>
2020-06-16 17:27:28 +0800

    @LoadBalanced: RestTemplate使得consumer通过serviceName访问provider具有RR的效果

--- cloud-consumer-order80/src/main/java/com/lei/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/lei/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.lei.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ public class ApplicationContextConfig {
     // http远程调用另外的服务, OrderController服务没有直接xxxService资源对象调用，所以要用RestTemplate远程调用;
     // 这个RestTemplate需要首先注入Spring容器: 用@Configuration和@Bean
     @Bean
+    @LoadBalanced // 给restTemplate负载均衡的能力，就可以通过服务名称，绕过服务后面多台主机，直接访问
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
